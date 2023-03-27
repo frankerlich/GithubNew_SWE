b6cd6cf4e8a96341b9f7e29f00f4ae9320c8e11c
frankerlich <frank.erlich34@gmail.com>
2023-03-27 09:51:50 +0200

    Neuer Kommentar Branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo Welt"); // Kommentae
+        System.out.println("Hallo Welt Branch"); // Kommentae
+        // Noch ein Kommentar
     }
 }
